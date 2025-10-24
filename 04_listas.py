6b41103c644ec0f62ef428fc5129be65e24a2294
Eddison Guillermo <stevenguillnavides@gmail.com>
2025-10-24 15:14:56 -0500

    listas clase 1.0

---
+++ 04_listas.py
@@ -0,0 +1,28 @@
+##  Clase 04 Listas ##
+
+## Una lista es un conjunto de elementos
+
+
+my_list = list()
+my_other_list = []
+
+print (len(my_list))
+
+my_list = [25, 54, 30, 30, 18]
+print (len(my_list))
+
+my_other_list = [25, 1.68, 'Eddison', 'Guillermo']
+
+print(type(my_list))
+print(type(my_other_list))
+
+
+
+print(my_other_list [0]) # Acceder a un dato de una lista segun su posicion 
+print(my_other_list [3])
+print(my_other_list [-1])
+print(my_other_list [-3])
+
+
+
+
